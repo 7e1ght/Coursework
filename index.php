d2a5713e0178769d0a6444990bfdccca82f7efda
Vlad <7e1ght.jm@gmail.com>
2019-05-13 02:07:35 +0300

    basic ready

--- index.php
+++ index.php
@@ -1,43 +1,54 @@
 <html>
 <head>
+
 	<meta charset="UTF-8">
-	<title>test</title>
+	<title>Камчатка</title>
+	<link rel="stylesheet" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet"> 
+
 </head>
 <body>
-	МДфв
-	<?php
-		require_once 'connection.php';
+	<header>
+		<div class="header_content">
 
-		$link = mysqli_connect($host, $user, $password, $database) 
-			or die("Ошибка " . mysqli_error($link));
+			<nav class="links">
+				<ul>
+					<li>
+						<a href="#">О нас</a>
+					</li>
+					<li>
+						<a href="#">Клиентам</a>
+					</li>
+					<li>
+						<a href="#">Полезная информация</a>
+					</li>
+				</ul>
+			</nav>
 
-		$query = "SELECT t.T_NAME, c.C_NAME, ci.CI_NAME, cl.DATA_START, cl.DATA_END, v.V_NAME
-					FROM CLIENTS cl
-					INNER JOIN TOURS t ON cl.TOUR = t.T_ID 
-					INNER JOIN COUNTRIES c ON cl.COUNTRY = c.C_ID
-					INNER JOIN CITIES ci ON cl.COUNTRY = ci.ID
-					INNER JOIN VEHICLES v ON cl.COUNTRY = v.V_ID";
+			<div class="login">
+				login
+			</div>
 
-		$result = mysqli_query($link, $query) or die("Ошибка " . mysqli_error($link));
+		</div>
+
+	</header>
+
+	<img src="img/logo.jpg" alt="" class="backHead">
+
+	<div class="content_wrapper">
+		<div class="content"> 
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod fuga voluptate numquam harum ut, rem, dicta doloribus aut dignissimos. Vel animi facilis neque tempora veritatis explicabo eligendi pariatur sequi, numquam doloremque. Nulla, ad dolores, aperiam et nisi quo, dolorum doloribus voluptates nam architecto mollitia libero sit, obcaecati quam voluptas. A magni, error mollitia delectus recusandae sapiente nisi ex voluptatibus odit? Et error, ipsa natus neque explicabo modi accusantium aut quam totam saepe, iste aliquam earum. Tenetur nesciunt amet nulla iure animi similique laborum qui necessitatibus commodi repellat harum officia optio id obcaecati, sint labore tempora eius eum aut et, possimus quasi nisi. Velit facere, iure quasi quisquam, magni assumenda perferendis harum, illum architecto fugit suscipit tempore illo minus omnis ab id in dolores animi sit dignissimos? Velit, asperiores, vero eligendi illo dignissimos ipsum atque sunt dolore necessitatibus provident maiores cupiditate cum excepturi, facere id. A laborum eos odit est ab quis, nemo in, veniam voluptate iusto odio id quos assumenda saepe at, reiciendis molestias, tenetur voluptates provident molestiae suscipit repudiandae consequuntur. Deserunt, iure maxime nulla id voluptate hic tempore nesciunt placeat atque esse voluptates pariatur explicabo obcaecati ducimus officia, quia cumque eveniet harum. Omnis impedit, reprehenderit provident itaque, voluptatibus sunt aliquid quisquam nesciunt consequuntur quo, repellat nihil aperiam repudiandae nulla minima quaerat saepe! Amet quasi nemo ratione necessitatibus aspernatur, eveniet doloremque deleniti! Asperiores provident, vero ea cum iste dolorem corporis quasi illo repudiandae, temporibus quidem, itaque similique ut id dignissimos qui nisi consectetur aliquid totam harum, veritatis necessitatibus unde earum. Sint ad nobis debitis sunt excepturi nulla, officia at ratione est harum veritatis impedit ullam ducimus commodi quos, voluptas, facere voluptatibus? Odio omnis, soluta! Fugiat vel autem modi quae illum excepturi veniam accusamus facilis non fugit velit nihil, repellendus magnam, tempore ipsum consectetur magni tempora quasi eius. Sit rerum asperiores officia illo corporis nesciunt corrupti tempore ea ex dignissimos dolor ipsum nostrum natus unde earum excepturi, laboriosam necessitatibus ipsa minima cumque, veritatis enim quibusdam, molestiae vero? Voluptate, optio. Vero mollitia ea libero repellat quisquam, sit ducimus! Dignissimos laboriosam unde facere alias ea dolorum numquam, ducimus neque itaque obcaecati, fuga esse id nesciunt, culpa quibusdam! Sequi veritatis ratione nemo laboriosam provident dolores quidem cupiditate quae eos aspernatur commodi praesentium, ipsum, debitis. Quibusdam in laudantium maiores quo sed consequuntur debitis eum ratione ipsam, inventore, ea. Laborum voluptate obcaecati blanditiis aliquam nemo optio eaque modi aspernatur rerum at quidem, cumque in cupiditate atque repudiandae quod. Repudiandae odio reiciendis at, nemo dicta eligendi necessitatibus officia consectetur minima iusto temporibus sit, ad soluta fuga ex saepe architecto dolor deserunt, autem dolorum incidunt? Voluptas sint facere ex, consectetur, temporibus nam. Fugiat ex alias animi ullam illo sed placeat cupiditate nulla, distinctio asperiores labore laudantium dignissimos illum aliquid adipisci quis rerum commodi, repudiandae maiores assumenda quo error facilis soluta ratione! Aliquid id at omnis nesciunt aut magnam hic voluptates tempora eaque ipsum quibusdam, earum quas odit, totam assumenda architecto amet dolore quidem ipsam dolores accusamus repudiandae asperiores reprehenderit fugit est! Eveniet quasi impedit totam veritatis, suscipit incidunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit vel voluptates dolorem eos. Optio nostrum modi ipsam magni obcaecati iusto voluptatem, pariatur sequi eligendi laboriosam! Sint, inventore repellendus quo labore velit? Odio iste, aspernatur! Accusamus iusto perferendis voluptate hic, inventore magnam nisi, nostrum in, delectus omnis consequatur, quas voluptatum totam! Quos natus possimus, inventore officiis, provident, reprehenderit nulla et tempore corrupti ex est omnis, totam! Praesentium veniam laborum dolor officiis consequatur obcaecati necessitatibus ex beatae molestiae itaque et aut architecto iusto ducimus quaerat voluptatibus suscipit, quam. Et nisi tenetur optio dolores quasi laboriosam quaerat error, natus cupiditate adipisci impedit cumque, labore asperiores qui perspiciatis assumenda? Fugit sed doloremque voluptas in! Doloremque, recusandae fuga esse nisi, repudiandae omnis impedit dolor dolores explicabo. Ducimus obcaecati reiciendis facere nulla corporis ea iusto quibusdam consequuntur ullam dolores voluptatum eaque expedita laudantium explicabo veritatis, minima reprehenderit sed, soluta quasi dolorum doloremque inventore velit at cupiditate quos! Quod minus expedita quae nisi praesentium dignissimos ullam voluptas dolore sequi! Et ullam cupiditate modi reiciendis molestiae minus cumque pariatur, labore unde accusamus! Maiores minus vitae ducimus repudiandae, doloremque dicta reprehenderit non. Fuga libero dignissimos iure illum in architecto atque magnam perferendis, minus tempore, id ducimus, quis nam, quo earum odit. Pariatur vero, eligendi voluptates aliquid assumenda ex natus doloremque! Velit fugit consectetur dolores accusamus, delectus consequatur impedit temporibus nulla nesciunt consequuntur? Molestiae asperiores hic sequi repellendus officia nisi obcaecati incidunt aliquid. Amet, debitis, facilis. Quia tempora saepe praesentium quam maxime rem, vitae fugit porro dignissimos similique nisi, nesciunt dicta excepturi incidunt explicabo voluptates voluptatum delectus exercitationem, perferendis fuga labore magni quibusdam sapiente laborum blanditiis! Sunt omnis fugiat unde, enim corporis quaerat iusto ducimus quia cumque consequatur veniam, totam quas dolore. At tempore commodi soluta! Alias sed, praesentium reiciendis illum aliquam atque. Quaerat omnis, pariatur doloremque fuga dolore voluptatibus quod nulla fugiat reprehenderit dicta facere, dolor possimus optio aliquam officiis tempore corporis excepturi, accusantium repellendus. Officiis ducimus laudantium illo dolor placeat, voluptatibus voluptas, laboriosam consectetur praesentium necessitatibus numquam optio, ratione repellendus delectus quisquam tempora maiores velit. Iure atque ipsa ullam ab deleniti obcaecati facilis debitis, quia labore vitae, aliquam cupiditate temporibus tempora. Nesciunt eaque saepe, error pariatur voluptatibus totam. Exercitationem aliquam quaerat officia veritatis unde. Aliquid facere quidem nostrum est autem natus reprehenderit, sapiente tenetur dolorem possimus labore recusandae cupiditate assumenda, blanditiis quo provident! Illum esse odio ullam aliquid mollitia? Magnam quidem, est saepe nihil expedita voluptatibus atque! Aliquid quas veniam, voluptas iste velit quae laudantium modi. Id suscipit aspernatur iusto a laboriosam alias, atque ut eveniet inventore numquam consectetur dolorem velit assumenda, ab impedit officia voluptatum necessitatibus illum. Perferendis doloremque aliquid iusto facilis consequuntur debitis est officia amet, tempore natus, velit nemo. Eos doloremque provident quis molestiae in magni labore maxime ex architecto itaque natus qui obcaecati, inventore, voluptatem voluptas harum vel ipsum nam facilis nostrum. Delectus officiis suscipit alias a commodi, cum aperiam incidunt sunt unde adipisci tempore repellat molestias sint, quam dolores, consequatur, officia. Fugit et laboriosam, laborum non ratione! Consectetur animi minus atque, illo alias.
+
+
+		</div>
+		<footer>Vlad</footer>
+	</div>
+
+	<script>
+		document.getElementsByClassName("content_wrapper")[0].style.top = screen.height+"px";
+	</script>
+
+
 	
-		if($result) {
-			$rows = mysqli_num_rows($result);
-
-			echo "<table><tr> <th>1</th> <th>2</th> <th>3</th> <th>4</th> <th>5</th> <th>6</th> <th>7</th> <th>8</th> <th>9</th> <th>10</th> </tr>";
-
-			for($i = 0; $i < $rows; $i++) {
-				$row = mysqli_fetch_row($result);
-		        echo "<tr>";
-		            for ($j = 0 ; $j < 6 ; ++$j) echo "<td>$row[$j]</td>";
-		        echo "</tr>";
-			}
-			echo "</table>";
-
-			mysqli_free_result($result);
-		}
-
-		mysqli_close($link);
-	?>
-
 </body>
 </html>
